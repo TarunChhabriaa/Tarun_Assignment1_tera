4807bd90707e498142d7ebe92852c49652ffb670
EC2 Default User <ec2-user@ip-172-31-46-241.ec2.internal>
2023-06-03 05:02:34 +0000

    My first commit

---
+++ modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Tarun Chhabria"
+    "App"   = "Web"
+    "Project" = "CLO835 Assignment"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "week4"
+}
