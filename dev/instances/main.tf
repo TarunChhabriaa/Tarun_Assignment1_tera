26c1d2eaf056cd1956e157479d2a9af386ba80b2
Tarun <tmchhabria@myseneca.ca>
2023-06-03 05:45:19 +0000

    opening port 8051,2,3

--- dev/instances/main.tf
+++ dev/instances/main.tf
@@ -85,6 +85,32 @@ resource "aws_security_group" "my_sg" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  
+  ingress {
+    description      = "HTTP from everywhere"
+    from_port        = 8051
+    to_port          = 8051
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+ ingress {
+    description      = "HTTP from everywhere"
+    from_port        = 8052
+    to_port          = 8052
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+ ingress {
+    description      = "HTTP from everywhere"
+    from_port        = 8053
+    to_port          = 8053
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 
   egress {
     from_port        = 0
